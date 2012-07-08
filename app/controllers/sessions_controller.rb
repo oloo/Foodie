f5e6a150cf2f60b283f5fff277b580040d8524cb
Ray Besiga <raybesiga@gmail.com>
2012-07-09 00:23:22 +0300

    User Model edit

--- app/controllers/sessions_controller.rb
+++ app/controllers/sessions_controller.rb
@@ -1,6 +1,6 @@
 class SessionsController < ApplicationController
 	def create
-		#user = User.from_omniauth(env["ominauth.auth"])
+		user = User.from_omniauth(env["ominauth.auth"])
 		session[:user_id] = user.user_id
 		redirect_to root_url
 	end
