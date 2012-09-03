d59332888afb08d8754fefb388dac90d9170befd
Ray Besiga <raybesiga@gmail.com>
2012-09-03 23:14:00 +0300

    Fixing Facebook Auth

--- config/initializers/omniauth.rb
+++ config/initializers/omniauth.rb
@@ -1,5 +1,5 @@
 OmniAuth.config.logger = Rails.logger
 
 Rails.application.config.middleware.use OmniAuth::Builder do
-  provider :facebook, "217697998352875", "2104af93cedd0c88da37c5332afe4aef", :display => "popup"	
+  provider :facebook, ENV['217697998352875'], ENV['2104af93cedd0c88da37c5332afe4aef'], {:client_options => {:ssl => {:ca_path => "/etc/ssl/certs"}}}, :display => "popup"	
 end
